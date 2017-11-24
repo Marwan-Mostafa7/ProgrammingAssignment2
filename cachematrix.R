68c3f324ae72b2ecaba2de21839b98505bf530d3
Marwan <Marwan_Elghitany@yahoo.com>
2017-11-24 21:40:58 +0200

    Cashing inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing inverse of a long matrix once, and saving this data
+## in the cashe memory to save doing time of computation that have been done before
+## Assuming the Data of matrix is not changing
 
-## Write a short comment describing this function
+
+
+##  matrix  -> list containing a function to
+
+#1) set the value of the vector
+#2) get the value of the vector
+#3) set the value of the mean
+#4) get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  
+  # cashed inverse initialized to null
+  cashed_inv <- NULL
+  
+  
+  set_matrix <- function(y){
+    x <<- y
+    cashed_inv <<- NULL
+  }
+  
+  get_matrix <- function() x
+  
+  set_inverse_mat <- function(calc_inverse) cashed_inv<<-calc_inverse
+  get_inverse_mat <- function() cashed_inv
+  
+  list(set=set_matrix, get=get_matrix, setinverse=set_inverse_mat, getinverse=get_inverse_mat)
 }
 
 
-## Write a short comment describing this function
+
+# matrix -> matrix
+
+## Computes the inverse of the consumed matrix
+## if inverse is already exist in cashe the saved data is directly returned
+## without further computaions
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  
+  inv <- x$getinverse()
+  
+  ## Checking if value is in cache memory or not
+  if(!is.null(inv)){
+    print("Computed from Cashe")
+    return (inv)
+  }
+  
+  data <- x$get()
+  
+  inv <- solve(data)
+  
+  x$setinverse(inv)
+  
+  return(inv)
 }
